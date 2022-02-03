29a4b8750ff45ea198aa295ce7c6ca1dddab5c3a
benjamin <25880887+Benjamin-KLMG@users.noreply.github.com>
2022-02-03 18:30:25 +0100

    introduction of the class data

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import sys
-import tools as tls
+from tools import Data
 
 def main():
   file_name = "data/input/song_data.csv"
@@ -8,10 +8,13 @@ def main():
   file_name = sys.argv[1]
   limit = int(sys.argv[2])
 
-  dict_song,head = tls.read_and_precess_file(file_name)
-  sorted_dict_song = tls.sorted_data_by_Number_of_times(dict_song,limit)
-  tls.write_file(sorted_dict_song, head)
+  # dict_song,head = tls.read_and_precess_file(file_name)
+  # sorted_dict_song = tls.sorted_data_by_Number_of_times(dict_song,limit)
+  # tls.write_file(sorted_dict_song, head)
 
+  song_data = Data()
+  song_data.read_and_precess_file(file_name)
+  print(song_data.song_dict)
 
 if __name__ == "__main__":
     main()
